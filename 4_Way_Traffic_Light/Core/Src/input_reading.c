fa477b097c7a1cc13d5b44a4ed310449f4a66339
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-12-13 19:35:14 +0700

    add button read

--- 4_Way_Traffic_Light/Core/Src/input_reading.c
+++ 4_Way_Traffic_Light/Core/Src/input_reading.c
@@ -7,3 +7,86 @@
 
 
 #include "input_reading.h"
+//we aim to work with more than one buttons
+#define NO_OF_BUTTONS 				       4
+//time to register hold
+#define DURATION_FOR_BUTTON_HOLD		   3000
+
+#define BUTTON_IS_PRESSED                  GPIO_PIN_RESET
+#define BUTTON_IS_RELEASED                 GPIO_PIN_SET
+//the buffer that the final result is stored after
+
+//button ports and pins array
+GPIO_TypeDef * buttonPort[NO_OF_BUTTONS] = {
+	A0_GPIO_Port,
+	A1_GPIO_Port,
+	A2_GPIO_Port,
+	A3_GPIO_Port
+};
+
+uint16_t buttonPin[NO_OF_BUTTONS] = {
+	A0_Pin,
+	A1_Pin,
+	A2_Pin,
+	A3_Pin
+};
+
+//debouncing
+static GPIO_PinState buttonBuffer[NO_OF_BUTTONS] = {BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED};
+//we define three buffers for debouncing
+static GPIO_PinState debounceButtonBuffer0[NO_OF_BUTTONS] = {BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED};
+static GPIO_PinState debounceButtonBuffer1[NO_OF_BUTTONS] = {BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED};
+static GPIO_PinState debounceButtonBuffer2[NO_OF_BUTTONS] = {BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED, BUTTON_IS_RELEASED};
+//we define a flag for a button pressed more than 1 second.
+static uint8_t flagForButtonPressed[NO_OF_BUTTONS];
+static uint8_t flagForButtonHold[NO_OF_BUTTONS];
+//we define counter for automatically increasing the value
+//after the button is pressed more than 1 second.
+static uint16_t counterForButtonHold[NO_OF_BUTTONS] = {0, 0, 0};
+
+void buttonRead(void){
+	for(int i = 0; i < NO_OF_BUTTONS; i++) {
+		debounceButtonBuffer2[i] = debounceButtonBuffer1[i];
+		debounceButtonBuffer1[i] = debounceButtonBuffer0[i];
+		debounceButtonBuffer0[i] = HAL_GPIO_ReadPin(buttonPort[i], buttonPin[i]);
+		if((debounceButtonBuffer2[i] == debounceButtonBuffer1[i]) && (debounceButtonBuffer1[i] == debounceButtonBuffer0[i])) {
+			if(buttonBuffer[i] != debounceButtonBuffer0[i]) {
+				buttonBuffer[i] = debounceButtonBuffer0[i];
+				if(buttonBuffer[i] == BUTTON_IS_PRESSED) {
+					counterForButtonHold[i] = DURATION_FOR_BUTTON_HOLD;
+					if(SYSTEM_DELAY > 0) counterForButtonHold[i] /= SYSTEM_DELAY;
+
+					flagForButtonPressed[i] = 1;
+				}
+				else {
+					flagForButtonHold[i] = 0;
+				}
+			}
+			else {
+				if(buttonBuffer[i] == BUTTON_IS_PRESSED) {
+					counterForButtonHold[i]--;
+					if(counterForButtonHold[i] == 0) {
+						flagForButtonHold[i] = 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+int isButtonPressed(int index) {
+	if(index < 0 || index > NO_OF_BUTTONS) return 0;
+	if(flagForButtonPressed[index] == 1) {
+		flagForButtonPressed[index] = 0;
+		return 1;
+	}
+	return 0;
+}
+
+int isButtonHold(int index) {
+	if(index < 0 || index > NO_OF_BUTTONS) return 0;
+	if(flagForButtonHold[index] == 1) {
+		return 1;
+	}
+	return 0;
+}
