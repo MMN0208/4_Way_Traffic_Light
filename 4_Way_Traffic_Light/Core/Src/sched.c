b30fc4fa40df1ec5e90492757c69ab60338adc63
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-12-24 14:42:49 +0700

    Update scheduler

--- 4_Way_Traffic_Light/Core/Src/sched.c
+++ 4_Way_Traffic_Light/Core/Src/sched.c
@@ -8,8 +8,9 @@
 
 #include "sched.h"
 
+#define	HEAD	0
+
 sTask SCH_tasks_G[SCH_MAX_TASKS];
-uint32_t Current = 0;
 uint32_t Size = 0;
 
 void SCH_Init(void) {
@@ -29,6 +30,7 @@ uint32_t SCH_Add_Task(void (* pFunction)(void), uint32_t DELAY, uint32_t PERIOD)
 	if(SYSTEM_DELAY > 0) {
 		DELAY /= SYSTEM_DELAY;
 	}
+
 	for(; Index < Size && flag == 0; Index++) {
 		if(DELAY > SCH_tasks_G[Index].Delay) {
 			DELAY -= SCH_tasks_G[Index].Delay;
@@ -36,22 +38,23 @@ uint32_t SCH_Add_Task(void (* pFunction)(void), uint32_t DELAY, uint32_t PERIOD)
 		else {
 			flag = 1;
 			SCH_tasks_G[Index].Delay -= DELAY;
+			if(SCH_tasks_G[Index].Delay == 0) {
+				SCH_tasks_G[Index].RunMe = 1;
+			}
 		}
 	}
+
 	if(flag == 1) {
 		Index -= 1;
 		for(uint32_t Temp = Size; Temp > Index; Temp--) {
-			SCH_tasks_G[Temp].pTask = SCH_tasks_G[Temp - 1].pTask;
-			SCH_tasks_G[Temp].Delay = SCH_tasks_G[Temp - 1].Delay;
-			SCH_tasks_G[Temp].Period = SCH_tasks_G[Temp - 1].Period;
-			SCH_tasks_G[Temp].RunMe = SCH_tasks_G[Temp - 1].RunMe;
-			SCH_tasks_G[Temp].TaskID = SCH_tasks_G[Temp - 1].RunMe + 1;
+			SCH_tasks_G[Temp] = SCH_tasks_G[Temp - 1];
+			SCH_tasks_G[Temp].TaskID += 1;
 		}
 	}
 	SCH_tasks_G[Index].pTask = pFunction;
 	SCH_tasks_G[Index].Delay = DELAY;
 	SCH_tasks_G[Index].Period = PERIOD;
-	SCH_tasks_G[Index].RunMe = 0;
+	SCH_tasks_G[Index].RunMe = (DELAY == 0) ? 1 : 0;
 	SCH_tasks_G[Index].TaskID = Index;
 
 	Size += 1;
@@ -62,34 +65,34 @@ uint32_t SCH_Add_Task(void (* pFunction)(void), uint32_t DELAY, uint32_t PERIOD)
 void SCH_Delete_Task(uint32_t TASK_INDEX) {
 	if(SCH_tasks_G[TASK_INDEX].pTask == 0) return;
 
+	SCH_tasks_G[TASK_INDEX + 1].Delay += SCH_tasks_G[TASK_INDEX].Delay;
+
 	for(uint32_t Temp = TASK_INDEX; Temp < Size - 1; Temp++) {
-		SCH_tasks_G[Temp].pTask = SCH_tasks_G[Temp + 1].pTask;
-		SCH_tasks_G[Temp].Delay = SCH_tasks_G[Temp + 1].Delay;
-		SCH_tasks_G[Temp].Period = SCH_tasks_G[Temp + 1].Period;
-		SCH_tasks_G[Temp].RunMe = SCH_tasks_G[Temp + 1].RunMe;
-		SCH_tasks_G[Temp].TaskID = SCH_tasks_G[Temp + 1].TaskID - 1;
+		SCH_tasks_G[Temp] = SCH_tasks_G[Temp + 1];
+		SCH_tasks_G[Temp].TaskID -= 1;
 	}
 
 	Size -= 1;
 }
 
 void SCH_Dispatch_Tasks(void) {
-	if(SCH_tasks_G[0].RunMe == 1) {
-		(*SCH_tasks_G[0].pTask)();
+	if(SCH_tasks_G[HEAD].RunMe == 1) {
+		(*SCH_tasks_G[HEAD].pTask)();
 
-		if(SCH_tasks_G[0].Period > 0) {
-			SCH_Add_Task(SCH_tasks_G[Current].pTask, SCH_tasks_G[Current].Period, SCH_tasks_G[Current].Period);
+		sTask temp = SCH_tasks_G[HEAD];
+
+		SCH_Delete_Task(HEAD);
+
+		if(temp.Period > 0) {
+			SCH_Add_Task(temp.pTask, temp.Period, temp.Period);
 		}
-
-		SCH_Delete_Task(0);
 	}
 }
 
 void SCH_Update(void) {
-	if(SCH_tasks_G[0].Delay == 0) {
-		SCH_tasks_G[0].RunMe = 1;
-	}
-	else {
-		SCH_tasks_G[0].Delay--;
+	if(SCH_tasks_G[HEAD].Delay > 0) SCH_tasks_G[HEAD].Delay--;
+
+	if(SCH_tasks_G[HEAD].Delay == 0) {
+		SCH_tasks_G[HEAD].RunMe = 1;
 	}
 }
